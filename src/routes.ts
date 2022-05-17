c9fc13f97c7c28de50537a92676c68b4593c65bf
guibrbs <200037994@aluno.unb.br>
2022-05-16 21:28:22 -0300

    :floppy_disk: Adding try catch error

--- src/routes.ts
+++ src/routes.ts
@@ -3,25 +3,28 @@ import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-ada
 import { PrismaFeedbacksRepository } from './repositories/prisma/prisma-feedbacks-repository';
 import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
-
 export const routes = express.Router()
 
 routes.post('/feedbacks', async (req, res) => {
-    const { type, comment, screenshot} = req.body;
+    const { type, comment, screenshot } = req.body;
+    try {
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+        const nodemailerMailAdapter = new NodemailerMailAdapter()
 
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-    const nodemailerMailAdapter = new NodemailerMailAdapter()
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbacksRepository,
+            nodemailerMailAdapter
+        )
 
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-        prismaFeedbacksRepository,
-        nodemailerMailAdapter
-    )
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot
+        })
 
-    await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot
-    })
-
-    return res.status(201).send()
+        return res.status(201).send()
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send
+    }
 })
